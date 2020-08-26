5afaf23bd865a667448863f55694a0ae97658b14
Sara Shahin <sarashahin@Saras-MacBook-Air.local>
2020-08-26 16:19:00 +0100

    Main navigation made responsive and favicon added

--- resources/js/script.js
+++ resources/js/script.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
     });
 
 
-    /* Animations on scroll */
+    /* Trigger Animations on scroll */
     /* fade in content in the #about section */
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');
@@ -87,6 +87,55 @@ $(document).ready(function() {
         $('.js--wp-3').addClass('animated pulse');
     }, {
         offset: '50%'
+    });
+
+
+
+    /* Mobile navigation */
+    /* This code was found on various forums and modified by me */
+
+
+    $('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        //Gets the class name of the element that triggered the event
+        var clicked = element.target.className;
+
+        //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+        if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black')
+            return;
+
+        //Opens and closes the menu
+        if ($(window).width() < 768){
+            nav.slideToggle(200);
+        }
+
+        //Changes icon states of the menu button
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+
+    $(window).resize(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 767){
+            nav.css("display", "block");
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
     });
 
   }); //end of $(document).ready(function(){}
